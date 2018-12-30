0badfba637504f53d802eab266ae456efe3af64a
Eachen <695089605@qq.com>
2018-12-30 09:31:30 +0800

    | 2018.12.30 | 848(medium) | yes | 96 |

--- String/842#Split Array into Fibonacci Sequence.py
+++ String/842#Split Array into Fibonacci Sequence.py
@@ -48,8 +48,8 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(S)
-        for i in xrange(1, 11):
-            for j in xrange(1, 11):
+        for i in range(1, 11):
+            for j in range(1, 11):
                 if i + j >= n:
                     break
                 L = self.buildFibo(S, i, j)
