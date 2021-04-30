842bc5e3c62dd578c54096363b1167e5d83b24f7
eachen <695089605@qq.com>
2021-04-30 15:57:52 +0800

    14_2 - 16

---
+++ SwordOffer/SwordOffer_16.py
@@ -0,0 +1,54 @@
+# 实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，xn）。不得使用库函数，同时不需要考虑大数问题。
+#
+#  
+#
+# 示例 1：
+#
+# 输入：x = 2.00000, n = 10
+# 输出：1024.00000
+# 示例 2：
+#
+# 输入：x = 2.10000, n = 3
+# 输出：9.26100
+# 示例 3：
+#
+# 输入：x = 2.00000, n = -2
+# 输出：0.25000
+# 解释：2-2 = 1/22 = 1/4 = 0.25
+#  
+#
+# 提示：
+#
+# -100.0 < x < 100.0
+# -231 <= n <= 231-1
+# -104 <= xn <= 104
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/shu-zhi-de-zheng-shu-ci-fang-lcof
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        """
+        这个方法会超时，不正确
+        """
+        if n == 0:
+            return 1
+        if n < 0:
+            return self.myPow(1/x, -n)
+        res = 1
+        while n > 0:
+            res *= x
+            n -= 1
+        return res
+
+    def myPow_1(self, x: float, n: int) -> float:
+        if x == 0:
+            return 0
+        res = 1
+        if n < 0:
+            x, n = 1 / x, -n
+        while n:
+            if n & 1:
+                res *= x
+            x *= x
+            n >>= 1
+        return res
