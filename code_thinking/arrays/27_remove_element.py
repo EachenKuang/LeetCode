1910896ed4fe4295e2a26cfbdf59d62e9358b9cc
eachenkuang <695089605@qq.com>
2023-08-13 21:58:23 +0800

    add arrays

---
+++ code_thinking/arrays/27_remove_element.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/8/13 21:25
+# @Author  : eachenkuang
+# @Email   : eachenkuang@foxmail.com
+# @File    : 27_remove_element.py
+#
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        """
+        暴力法
+        时间复杂度：O(n^2)
+        空间复杂度：O(1)
+        """
+
+        i, l = 0, len(nums)
+        while i < l:
+            if nums[i] == val:  # 找到等于目标值的节点
+                for j in range(i + 1, l):  # 移除该元素，并将后面元素向前平移
+                    nums[j - 1] = nums[j]
+                l -= 1
+                i -= 1
+            i += 1
+        return l
+
+    def removeElement_2(self, nums: List[int], val: int) -> int:
+        """
+        双指针法
+        时间复杂度：O(n)
+        空间复杂度：O(1)
+        """
+        fast = slow = 0
+        while fast < len(nums):
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
---
+++ code_thinking/arrays/704_binary_search.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/8/13 20:58
+# @Author  : eachenkuang
+# @Email   : eachenkuang@foxmail.com
+# @File    : 704_binary_search.py
+# https://leetcode.cn/problems/binary-search/
+# 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+#
+# 示例 1:
+#
+# 输入: nums = [-1,0,3,5,9,12], target = 9
+# 输出: 4
+# 解释: 9 出现在 nums 中并且下标为 4
+# 示例 2:
+#
+# 输入: nums = [-1,0,3,5,9,12], target = 2
+# 输出: -1
+# 解释: 2 不存在 nums 中因此返回 -1
+# 提示：
+#
+# 你可以假设 nums 中的所有元素是不重复的。
+# n 将在 [1, 10000]之间。
+# nums 的每个元素都将在 [-9999, 9999]之间。
+from typing import List
+
+
+class Solution:
+    """
+    使用二分法，需要同时满足两个体条件：
+    1、有顺序
+    2、无重复
+    """
+    def search(self, nums: List[int], target: int) -> int:
+        """
+        时间复杂度：O(log n)
+        空间复杂度：O(1)
+        [左闭右闭区间]
+        """
+        left, right = 0, len(nums) - 1  # 左闭右闭区间，while条件使用<=
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+        return -1
+
+    def search_2(self, nums: List[int], target: int) -> int:
+        """
+        时间复杂度：O(log n)
+        空间复杂度：O(1)
+        [左闭右开区间）
+        """
+        left, right = 0, len(nums)  # 左闭右开区间，while条件使用<
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid
+            else:
+                return mid
+        return -1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    nums = [-4, 5, 6, 10, 999]
+    target = 19
+    assert -1 == solution.search(nums, target)
+    assert -1 == solution.search_2(nums, target)
